e3f8a9a3d8aaee67767f439ee14157bd6d0c1f25
崔建国 <cuijgb@tmlake.com>
2022-10-11 16:40:37 +0800

    add header

--- server/http_header.go
+++ server/http_header.go
@@ -5,7 +5,8 @@ import "net/http"
 func (c *Server) CrossOrigin(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, X-Requested-By, If-Modified-Since, X-File-Name, X-File-Type, Cache-Control, Origin")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH")
 	w.Header().Set("Access-Control-Expose-Headers", "Authorization")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	//https://blog.csdn.net/yanzisu_congcong/article/details/80552155
 }
